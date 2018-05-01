a6f370bdf31a41356eb20d8e28721336d689c653
Tara Johnson <tara@madface.com>
2018-05-01 15:42:44 -0700

    Updated Q1 syntax and created shell for Q2

--- js/app.js
+++ js/app.js
@@ -4,16 +4,15 @@
 var userName = prompt('Hi! What is your name!');
 
 // Greet the user by name in an alert
-alert('Hi, ' + userName + ', thanks for stopping by!');
+alert('Hey, ' + userName + ', thanks for stopping by!');
 
 // Log that to the console
 console.log('The user said their name is ' + userName);
 
 // Ask the first guessing game question
-var answer1 = prompt('Do you think I have any siblings?');
+var answer1 = prompt('Do I have blue eyes?');
 
 // User guesses "yes/no" or "Y/N"
-
 // if (condition is true) {
 // do this stuff
 // } else {
@@ -23,12 +22,25 @@ var answer1 = prompt('Do you think I have any siblings?');
 // Tell user if they are right or wrong
 if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y'){
     // right answer
-    alert('You got it right!!!');
+    alert('Take another look. They\'re brown! :) \n\nLet\'s try another one.');
     console.log('The user got the correct answer to Q1 (yes)');
+//    var moreAboutMe = prompt('Are you ready to learn more about me?');
+//    if (moreAboutMe.toLowerCase() === 'yes' || moreAboutMe.toLocaleLowerCase() === 'y'){
+//        }
+
 } else {
     // wrong answer
-    alert('FAIL');
+    alert('That\'s right! \n\nLet\'s try another one.');
     console.log('The user got the wrong answer to Q1 (no)');
 }
 
-// Next question
+// Ask the second question
+var answer2 = prompt('');
+
+// User guesses "yes/no" or "Y/N"
+
+// if (condition is true) {
+// do this stuff
+// } else {
+// do this stuff
+// }
