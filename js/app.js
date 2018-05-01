29425983d2d916143f495749e48f3d6a85a66ed3
Tara Johnson <tara@madface.com>
2018-05-01 16:00:22 -0700

    Q2 works and added stub code for questions 3-5

--- js/app.js
+++ js/app.js
@@ -12,35 +12,59 @@ console.log('The user said their name is ' + userName);
 // Ask the first guessing game question
 var answer1 = prompt('Do I have blue eyes?');
 
-// User guesses "yes/no" or "Y/N"
-// if (condition is true) {
-// do this stuff
-// } else {
-// do this stuff
-//}
-
 // Tell user if they are right or wrong
 if (answer1.toLowerCase() === 'yes' || answer1.toLowerCase() === 'y'){
     // right answer
-    alert('Take another look. They\'re brown! :) \n\nLet\'s try another one.');
+    alert('Take another look. They\'re brown! :)');
     console.log('The user got the correct answer to Q1 (yes)');
-//    var moreAboutMe = prompt('Are you ready to learn more about me?');
-//    if (moreAboutMe.toLowerCase() === 'yes' || moreAboutMe.toLocaleLowerCase() === 'y'){
-//        }
-
 } else {
     // wrong answer
-    alert('That\'s right! \n\nLet\'s try another one.');
+    alert('That\'s right!');
     console.log('The user got the wrong answer to Q1 (no)');
 }
 
-// Ask the second question
-var answer2 = prompt('');
+// Ask the second guessing game question
+var answer2 = prompt('Am I from Seattle?');
 
-// User guesses "yes/no" or "Y/N"
+// Tell user if they are right or wrong
+if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y'){
+    // Right answer to Q2
+    alert('Nope. I grew up in Cleveland, Ohio. \nI\'ve been in Seattle for 12 years though!');
+    console.log('The user got the wrong answer to Q2 (no)');
+} else {
+    // Wrong answer to Q2
+    alert('That\'s right! I grew up in Cleveland, Ohio.');
+    console.log('The user got the correct answer to Q2 (yes)');    
+}
+
+// Ask the third guessing game question
+
+// Tell user if they are right or wrong
 
 // if (condition is true) {
 // do this stuff
 // } else {
 // do this stuff
 // }
+
+
+// Ask the fourth guessing game question
+
+// Tell user if they are right or wrong
+
+// if (condition is true) {
+// do this stuff
+// } else {
+// do this stuff
+// } 
+
+
+// Ask the fifth guessing game question
+
+// Tell user if they are right or wrong
+
+// if (condition is true) {
+// do this stuff
+// } else {
+// do this stuff
+//}
