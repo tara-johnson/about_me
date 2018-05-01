36bc53f1134f5a8c2a3fe79a1ed3b57b98378994
Tara Johnson <tara@madface.com>
2018-05-01 14:20:53 -0700

    Pushing app.js with Q1 working

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
 'use strict';
 
-alert('IT IS ALIVE');
+// Ask for user's name
+var userName = prompt('Hi! What is your name!');
+
+// Greet the user by name in an alert
+alert('Hi, ' + userName + ', thanks for stopping by!');
+
+// Log that to the console
+console.log('The user said their name is ' + userName);
+
+// Ask the first guessing game question
+var answer1 = prompt('Do you think I have any siblings?');
+
+// User guesses "yes/no" or "Y/N"
+
+// if (condition is true) {
+// do this stuff
+// } else {
+// do this stuff
+//}
+
+// Tell user if they are wrong or right
+if (answer === 'yes' || answer1 === 'y'){
+    // right answer
+    alert('You got it right!!!');
+    console.log('The user got the correct answer to Q1 (yes)');
+} else {
+    // wrong answer
+    alert('FAIL');
+    console.log('The user got the wrong answer to Q1 (no)');
+}
+
+// Next question
