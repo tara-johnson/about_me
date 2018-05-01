377896eaa8b594f70bd52f9b14d84a29d5c42921
Tara Johnson <tara@madface.com>
2018-05-01 16:11:18 -0700

    Q3 works

--- js/app.js
+++ js/app.js
@@ -30,23 +30,26 @@ var answer2 = prompt('Am I from Seattle?');
 if (answer2.toLowerCase() === 'yes' || answer2.toLowerCase() === 'y'){
     // Right answer to Q2
     alert('Nope. I grew up in Cleveland, Ohio. \nI\'ve been in Seattle for 12 years though!');
-    console.log('The user got the wrong answer to Q2 (no)');
+    console.log('The user got the correct answer to Q2 (yes)');
 } else {
     // Wrong answer to Q2
     alert('That\'s right! I grew up in Cleveland, Ohio.');
-    console.log('The user got the correct answer to Q2 (yes)');    
+    console.log('The user got the wrong answer to Q2 (no)');    
 }
 
 // Ask the third guessing game question
+var answer3 = prompt('Have I lived in more than 3 states?');
 
 // Tell user if they are right or wrong
-
-// if (condition is true) {
-// do this stuff
-// } else {
-// do this stuff
-// }
-
+if (answer3.toLowerCase() === 'yes' || answer3.toLowerCase () === 'y'){
+    // Right answer to Q3
+    alert('It\s true! I\'ve lived in Ohio, Michigan, New York, and Washington.');
+    console.log('The user got the correct answer to Q3 (yes)');    
+} else {
+    // Wrong answer to Q3
+    alert('Wrong! Close though. I\'ve lived in four states - Ohio, Michigan, New York, and Washington.');
+    console.log('The user got the wrong answer to Q3 (no)');
+}
 
 // Ask the fourth guessing game question
 
